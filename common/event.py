2c5cef3cd6de6c453373d0a6dc35be326d2d1ed5
tdijital <61374259+tdijital@users.noreply.github.com>
2021-11-02 17:55:22 -0700

    Code style fix

--- common/event.py
+++ common/event.py
@@ -2,16 +2,16 @@
 class Event(object):
   
     def __init__(self):
-        self.__eventhandlers = []
+        self._eventhandlers = []
   
     def __iadd__(self, handler):
-        self.__eventhandlers.append(handler)
+        self._eventhandlers.append(handler)
         return self
   
     def __isub__(self, handler):
-        self.__eventhandlers.remove(handler)
+        self._eventhandlers.remove(handler)
         return self
   
     def __call__(self, *args, **keywargs):
-        for eventhandler in self.__eventhandlers:
+        for eventhandler in self._eventhandlers:
             eventhandler(*args, **keywargs)
